9050035c4d0c9f3ed6e9af35279649aba1e76be2
filipelenfers <filipelenfers@gmail.com>
2019-10-06 09:25:14 -0300

    addes interprester config & links to start stop

--- scripts/installZeppelin.sh
+++ scripts/installZeppelin.sh
@@ -3,10 +3,14 @@
 cd /vagrant
 
 #uncompress zeppelin to user home
-tar xvf deps/zeppelin-0.7.3-bin-all.tgz -C /home/vagrant
-
-#Change owner to vagrant user
-chown vagrant -R zeppelin-0.7.3-bin-all/
+tar xf deps/zeppelin-0.7.3-bin-all.tgz -C /home/vagrant
 
 #Copy configuration file already adjusted to AWS Glue needs
 cp -f deps/interpreter.json /home/vagrant/zeppelin-0.7.3-bin-all/conf/interpreter.json
+
+#Change owner to vagrant user
+chown vagrant -R /home/vagrant/zeppelin-0.7.3-bin-all/
+
+#Add start&stop scripts to path
+ln -s /vagrant/scripts/startZeppelin.sh /usr/local/bin/startZeppelin
+ln -s /vagrant/scripts/stopZeppelin.sh /usr/local/bin/stopZeppelin
