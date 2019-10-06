a16a7d5320f186d30ca9dee1ab10cb1c94a82e6a
filipelenfers <filipelenfers@gmail.com>
2019-10-06 09:30:17 -0300

    adding echos to show progress

--- scripts/installZeppelin.sh
+++ scripts/installZeppelin.sh
@@ -3,14 +3,22 @@
 cd /vagrant
 
 #uncompress zeppelin to user home
+echo "Uncopressing Apache Zeppelin..."
 tar xf deps/zeppelin-0.7.3-bin-all.tgz -C /home/vagrant
+echo "Done."
 
 #Copy configuration file already adjusted to AWS Glue needs
+echo "Applying configs for AWS Glue..."
 cp -f deps/interpreter.json /home/vagrant/zeppelin-0.7.3-bin-all/conf/interpreter.json
+echo "Done."
 
 #Change owner to vagrant user
+echo "Changing permissions to vagrant user..."
 chown vagrant -R /home/vagrant/zeppelin-0.7.3-bin-all/
+echo "Done."
 
 #Add start&stop scripts to path
+echo "Creating startZeppelon && stopZeppelin..."
 ln -s /vagrant/scripts/startZeppelin.sh /usr/local/bin/startZeppelin
 ln -s /vagrant/scripts/stopZeppelin.sh /usr/local/bin/stopZeppelin
+echo "Done."
